cf3c8966442f070b3193099b9accdc6b7817f9ae
Laurel Amah <laurelngumamah@gmail.com>
2025-05-22 12:29:01 +0100

    Second commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,8 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="https://unpkg.com/flowbite@1.6.5/dist/flowbite.min.js"></script>
+
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
